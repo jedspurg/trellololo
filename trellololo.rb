4bec5ee7f75ee77a1e8cc004c2c9ad32e7749b0d
Jeramy Spurgeon <jeramy.d.spurgeon@gmail.com>
2016-08-12 11:49:02 -0400

    more debugging love

--- trellololo.rb
+++ trellololo.rb
@@ -1,20 +1,10 @@
 require 'sinatra'
 
-set :show_exceptions, true
-
-log = File.new("sinatra.log", "a")
-STDOUT.reopen(log)
-STDERR.reopen(log)
-
-before do
-  puts '[Params]'
-  p params
-end
-
 get '/' do
   'Hello world!'
 end
 
 post '/trello/callback' do
-
+  puts '[Params]'
+  p params
 end
